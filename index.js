2d2242a05536f5961522eaa23a2edf7698a3d1f1
Jonathan McDonald <Jonathan@Jonathan.local>
2019-04-05 11:44:49 -0600

    initial styling

---
+++ index.js
@@ -0,0 +1,8 @@
+const composer = document.querySelector('#composer')
+const piece = document.querySelector('#piece')
+const tempo = document.querySelector('#tempo')
+const notes = document.querySelector('#notes')
+
+function addToList() {
+  console.log(composer)
+}

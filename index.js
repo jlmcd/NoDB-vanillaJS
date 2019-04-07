e8e148d4dc791f7e70a801d7ffe5b6e9e737fa5d
Jonathan McDonald <jonathanlmcd@gmail.com>
2019-04-06 22:41:40 -0600

    add item now clears inputs, adds to assignmentsArr

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
+// Inputs
 const assignments = document.querySelector('#assignments')
 const composer = document.querySelector('#composer')
 const piece = document.querySelector('#piece')
 const tempo = document.querySelector('#tempo')
 const notes = document.querySelector('#notes')
 
+const assignmentsArr = []  // for keeping track of the entire list
+
+
 function addToList() {
+  // Create new elements and containers for them
   const listItem = document.createElement('div')
   const composerEl = document.createElement('h3')
   const pieceEl = document.createElement('h2')
   const tempoEl = document.createElement('h3')
   const notesEl = document.createElement('h3')
+
+  const buttonGroup = document.createElement('div')
   const deleteButton = document.createElement('button')
   const editButton = document.createElement('button')
 
+  // Give classes to element containers
   listItem.className = 'list-item'
-  
+  buttonGroup.className = 'item-buttons'
+
+  // Set inner text of elements to be inputs
   composerEl.innerText = composer.value
   pieceEl.innerText = piece.value
   tempoEl.innerText = tempo.value
@@ -22,9 +32,27 @@ function addToList() {
   deleteButton.innerText = 'Delete'
   editButton.innerText = 'Edit'
 
-  listItem.append(composerEl, pieceEl, tempoEl, notesEl, deleteButton, editButton)
+  // Group elements together, and render them
+  buttonGroup.append(deleteButton, editButton)
+  listItem.append(composerEl, pieceEl, tempoEl, notesEl, buttonGroup)
   assignments.append(listItem)
+
+  // Add item to assignments array
+  assignmentsArr.push({
+    composer: composer.value, 
+    piece: piece.value, 
+    tempo: tempo.value,
+    notes: notes.value
+  })
+
+  // Clear inputs
+  assignments.value = null
+  composer.value = null
+  piece.value = null
+  tempo.value = null
+  notes.value = null
 }
+
 
 function removeFromList() {
   
